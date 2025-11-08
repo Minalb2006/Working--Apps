c1d6a4e583312bffd662c609b1d77707a182ec45
Minalb2006 <minalborkar0012@gmail.com>
2025-11-08 23:41:59 +0530

    zhal re baba aaj ch

---
+++ minal.js
@@ -0,0 +1,5 @@
+document.getElementById("hi").addEventListener("dbclick", hello);
+function hello() {
+    alert("get the feedback");
+// document.getElementById("he").innerHTML = "Hello everyone";
+}
