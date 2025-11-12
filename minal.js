63da48cc69828664f214a6d5dfa2ac7c5193e4dd
Minalb2006 <minalborkar0012@gmail.com>
2025-11-12 22:54:05 +0530

    my own website

--- minal.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("hi").addEventListener("dbclick", hello);
-function hello() {
-    alert("get the feedback");
-// document.getElementById("he").innerHTML = "Hello everyone";
-}
